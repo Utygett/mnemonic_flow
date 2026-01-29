2b1fc321b18e6f22da27a2a698ba461c47f261f1
Синченко Дмитрий Георгиевич <dgsinchenko@sdnb-mac-00067.local>
2026-01-29 14:41:30 +0300

    fix(backend/frontend): MF-29: clean unused code

--- frontend/src/shared/lib/hooks/index.ts
+++ frontend/src/shared/lib/hooks/index.ts
@@ -1,5 +1,3 @@
 // src/shared/lib/hooks/index.ts
-export { useApiData } from './useApi'
-export { useStudySession } from './useStudySession'
 export { useDecks } from './useDecks'
 export { useGroupsDecksController } from './useGroupsDecksController'
